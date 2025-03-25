9427f351002b6745acf2f7d3a8a7e20e50ed3ed6
aandersn <alexander.a.anderson@ieee.org>
2025-03-25 10:49:27 -0700

    add sphinx prolog

--- CIMantic_Graphs_Tutorial/conf.py
+++ CIMantic_Graphs_Tutorial/conf.py
@@ -77,6 +77,37 @@ latex_engine = 'lualatex'
 # Configure nbsphinx
 nbsphinx_execute = 'auto'  # Set to 'auto', 'always', or 'never' as needed
 
+# Add CSS to hide cells with hide_input tag
+nbsphinx_prolog = """
+{% set docname = env.doc2path(env.docname, base=None) %}
+
+.. raw:: html
+
+    <style>
+        /* Hide input cells with hide_input tag */
+        div.nbinput.container div.prompt.container.hide_input + div.input_area {
+            display: none;
+        }
+        
+        /* Alternative selector that should work with newer nbsphinx */
+        .tag_hide_input .jp-CodeCell-inputWrapper {
+            display: none !important;
+        }
+        
+        /* For nbsphinx 0.8.0+ */
+        .tag_hide_input .nbinput {
+            display: none !important;
+        }
+    </style>
+"""
+
+# Configure tag-based cell filtering
+nbsphinx_preprocessor_config = {
+    'TagRemovePreprocessor': {
+        'enabled': True,
+        'remove_input_tags': {'hide_input'},
+    }
+}
 
 # Additional options for fine-grained control
 nbsphinx_execute_arguments = [
